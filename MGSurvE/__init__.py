e000ebca76c474b851d9ca2b00fcd88ecb3ffe12
chipdelmal <chipdelmal@gmail.com>
2021-11-03 08:43:58 -0700

    Adding conda environment

--- MGSurvE/__init__.py
+++ MGSurvE/__init__.py
@@ -1,5 +1,8 @@
 
-from .constants import *
 from .auxiliary import *
+from .colors import *
+from .constants import *
+from .kernels import *
 from .landscape import *
+from .matrices import *
 from .trap import *
--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-version = '0.0.1a0'
+version = '0.0.1a1'
 
 if __name__ == '__main__':
     print(version)
