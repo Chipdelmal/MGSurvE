2fcdee877362f9ffded33ede903d8e7462ae5a6f
chipdelmal <chipdelmal@gmail.com>
2021-12-06 08:26:36 -0800

    Pretty terminal print

--- MGSurvE/dev/landsExplore.sh
+++ MGSurvE/dev/landsExplore.sh
@@ -3,8 +3,12 @@
 PTH_O='/RAID5/marshallShare/MGS_Demos'
 LND='UNIF'
 
-for id in {1..5}
+LG='\033[1;34m'
+NC='\033[0m' # No Color
+
+for id in {1..4}
 do
+    echo -e "${LG}* Launched landscape: ${id} ${NC}"
     python Optim_dev.py  $PTH_O $LND $id
-    echo "Finished landscape: ${id}"
+    echo -e "${LG}* Finished landscape: ${id} ${NC}"
 done
