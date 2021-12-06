a5e80e691747442697f4748702288e8ff2f9906e
chipdelmal <chipdelmal@gmail.com>
2021-12-06 08:28:21 -0800

    Going to try parallel bash

--- MGSurvE/dev/landsExplore.sh
+++ MGSurvE/dev/landsExplore.sh
@@ -3,8 +3,12 @@
 PTH_O='/RAID5/marshallShare/MGS_Demos'
 LND='UNIF'
 
-for id in {1..5}
-do
-    python Optim_dev.py  $PTH_O $LND $id
+
+N=2
+(
+for id in {1..4}; do
+    ((i=i%N)); ((i++==0)) && wait
+    python Optim_dev.py  $PTH_O $LND $id &
     echo "Finished landscape: ${id}"
 done
+)
