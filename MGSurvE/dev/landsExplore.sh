85e025856d8e83a3b461d0665f78d372509dad76
chipdelmal <chipdelmal@gmail.com>
2021-12-06 08:37:38 -0800

    Terminarl print update

--- MGSurvE/dev/landsExplore.sh
+++ MGSurvE/dev/landsExplore.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+N=2
 PTH_O='/RAID5/marshallShare/MGS_Demos'
 LND='UNIF'
 
 LG='\033[1;34m'
 NC='\033[0m'
 
-N=2
 (
 for id in {100..104}; do
     ((i=i%N)); ((i++==0)) && wait
     echo -e "${LG}* Launched landscape: ${id} ${NC}"
     python Optim_dev.py  $PTH_O $LND $id &
-    echo -e "${LG}* Finished landscape: ${id} ${NC}"
 done
 )
