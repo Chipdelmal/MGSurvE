57b9930b1c2ac1be3b2f59193ae4485fbe4fdb86
chipdelmal <chipdelmal@gmail.com>
2021-12-06 10:23:28 -0800

    Making bash serial due to errors

--- MGSurvE/dev/landsExplore.sh
+++ MGSurvE/dev/landsExplore.sh
@@ -7,10 +7,10 @@ LND='UNIF'
 LG='\033[1;34m'
 NC='\033[0m'
 
-(
+# (
 for id in {50..54}; do
-    ((i=i%N)); ((i++==0)) && wait
+    # ((i=i%N)); ((i++==0)) && wait
     echo -e "${LG}* Launched landscape: ${id} ${NC}"
-    python Optim_dev.py  $PTH_O $LND $id &
+    python Optim_dev.py  $PTH_O $LND $id 
 done
-)
+# )
