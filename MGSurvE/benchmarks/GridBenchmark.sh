262c9372818544700d60f6914537694318f79478
chipdelmal <chipdelmal@gmail.com>
2021-12-07 13:54:44 -0800

    Server benchmark sync

--- MGSurvE/benchmarks/GridBenchmark.sh
+++ MGSurvE/benchmarks/GridBenchmark.sh
@@ -9,7 +9,7 @@ LG='\033[1;34m'
 NC='\033[0m'
 
 
-for pts in 25 50 ; do
+for pts in 25 50 100 200 300; do
     for trps in {1..5}; do
         for rep in {1..2}; do
             echo -e "${LG}* LND: ${trps} ${pts} ${rep} ${NC}"
