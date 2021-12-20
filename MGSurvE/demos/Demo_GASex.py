33f75c6369c829c613fcfbecaeebd83d6740ce7e
chipdelmal <chipdelmal@gmail.com>
2021-12-20 10:13:04 -0800

    Working on Sex-Based example

---
+++ MGSurvE/demos/Demo_GASex.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math
+import numpy as np
+import pandas as pd
+from os import path
+from sys import argv
+from copy import deepcopy
+import matplotlib.pyplot as plt
+from deap import base, creator, algorithms, tools
+import MGSurvE as srv
+import warnings
+warnings.filterwarnings('ignore', 'The iteration is not making good progress')
+
+
+(OUT_PTH, LND_TYPE, ID, TRPS_NUM) = ('./scratch/', 'UNIF', 'SX01', 6)
+###############################################################################
+# Generating Pointsets
+###############################################################################
+ptsNum = 200
+bbox = ((-100, 100), (-80, 80))
+xy = srv.ptsRandUniform(ptsNum, bbox).T
+points = pd.DataFrame({'x': xy[0], 'y': xy[1], 't': [0]*xy.shape[1]})
+###############################################################################
+# Defining Movement
+###############################################################################
+movementKernel = {
+    'Male': {
+        'kernelFunction': srv.zeroInflatedExponentialKernel,
+        'kernelParams': {'params': [.075, 1.0e-10, math.inf], 'zeroInflation': .5}
+    },
+    'Female': {
+        'kernelFunction': srv.zeroInflatedExponentialKernel,
+        'kernelParams': {'params': [.075, 1.0e-10, math.inf], 'zeroInflation': .6}
+    }
+}
+###############################################################################
+# Defining Traps
+###############################################################################
+nullTraps = [0] * TRPS_NUM
+traps = pd.DataFrame({
+    'x': nullTraps, 'y': nullTraps, 'f': nullTraps, 't': nullTraps
+})
+tKernels = {
+    'Male': {
+        0: {'kernel': srv.exponentialDecay, 'params': {'A': .5, 'b': .1}}
+    },
+    'Female': {
+        0: {'kernel': srv.exponentialDecay, 'params': {'A': .3, 'b': .06}}
+    }
+}
+###############################################################################
+# Setting Landscape Up
+###############################################################################
+lndM = srv.Landscape(
+    points, traps=traps,
+    kernelFunction=movementKernel['Male']['kernelFunction'],
+    kernelParams=movementKernel['Male']['kernelParams'],
+    trapsKernels=tKernels['Male'], trapsRadii=[.1, ]
+)
+lndF = srv.Landscape(
+    points, traps=traps,
+    kernelFunction=movementKernel['Female']['kernelFunction'],
+    kernelParams=movementKernel['Female']['kernelParams'],
+    trapsKernels=tKernels['Female'], trapsRadii=[.1, ]
+)
+###############################################################################
+# Plot Landscape
+###############################################################################
+(fig, ax) = plt.subplots(1, 1, figsize=(15, 15), sharey=False)
+lndM.plotSites(fig, ax, size=100)
+lndM.plotMigrationNetwork(fig, ax, alphaMin=.3, lineWidth=50, lineColor='#03045e')
+lndF.plotMigrationNetwork(fig, ax, alphaMin=.3, lineWidth=35, lineColor='#03045e')
+srv.plotClean(fig, ax, frame=False, bbox=bbox)
+fig.savefig(
+    path.join(OUT_PTH, '{}_{}_CLN.png'.format(LND_TYPE, ID)), 
+    facecolor='w', bbox_inches='tight', pad_inches=0, dpi=300
+)
+###############################################################################
+# GA Settings
+############################################################################### 
+(weightMale, weightFemale) = (.5, 1)
+POP_SIZE = int(10*(lndM.trapsNumber*1.25))
+(MAT, MUT, SEL) = (
+    {'mate': .3, 'cxpb': 0.5}, 
+    {'mean': 0, 'sd': min([i[1]-i[0] for i in bbox])/5, 'mutpb': .4, 'ipb': .5},
+    {'tSize': 3}
+)
+lndM_GA = deepcopy(lndM)
+lndF_GA = deepcopy(lndF)
+###############################################################################
+# Registering Functions for GA
+############################################################################### 
+toolbox = base.Toolbox()
+creator.create("FitnessMin", 
+    base.Fitness, weights=(-1.0, )
+)
+creator.create("Individual", 
+    list, fitness=creator.FitnessMin
+)
+toolbox.register("initChromosome", srv.initChromosome, 
+    trapsCoords=lndM_GA.trapsCoords, 
+    fixedTrapsMask=trpMsk, coordsRange=bbox
+)
+toolbox.register("individualCreator", tools.initIterate, 
+    creator.Individual, toolbox.initChromosome
+)
+toolbox.register("populationCreator", tools.initRepeat, 
+    list, toolbox.individualCreator
+)
+toolbox.register(
+    "mate", tools.cxBlend, 
+    alpha=MAT['mate']
+)
+toolbox.register(
+    "mutate", tools.mutGaussian, 
+    mu=MUT['mean'], sigma=MUT['sd'], indpb=MUT['ipb']
+)
+# Select and evaluate ---------------------------------------------------------
+toolbox.register("select", 
+    tools.selTournament, tournsize=SEL['tSize']
+)
+toolbox.register("evaluate", 
+    srv.calcSexFitness, 
+    landscapeMale=lndM_GA,landscapeFemale=lndF_GA,
+    weightMale=weightMale, weightFemale=weightFemale,
+    optimFunction=srv.getDaysTillTrapped,
+    optimFunctionArgs={'outer': np.mean, 'inner': np.max}
+)
+###############################################################################
+# Registering functions for GA stats
+############################################################################### 
+pop = toolbox.populationCreator(n=POP_SIZE)
+hof = tools.HallOfFame(1)
+stats = tools.Statistics(lambda ind: ind.fitness.values)   
+stats.register("min", np.min)
+stats.register("avg", np.mean)
+stats.register("max", np.max)
+stats.register("best", lambda fitnessValues: fitnessValues.index(min(fitnessValues)))
+stats.register("traps", lambda fitnessValues: pop[fitnessValues.index(min(fitnessValues))])
+###############################################################################
+# Optimization Cycle
+############################################################################### 
+(pop, logbook) = algorithms.eaSimple(
+    pop, toolbox, cxpb=MAT['cxpb'], mutpb=MUT['mutpb'], ngen=GENS, 
+    stats=stats, halloffame=hof, verbose=VERBOSE
+)
+# Update with best results ----------------------------------------------------
+minFits= logbook.select("min")
+lndM.updateTrapsCoords(np.reshape(hof[0], (-1, 2)))
+lndF.updateTrapsCoords(np.reshape(hof[0], (-1, 2)))
+srv.dumpLandscape(lndM, OUT_PTH, '{}_{}_M_TRP'.format(LND_TYPE, ID))
+srv.dumpLandscape(lndF, OUT_PTH, '{}_{}_F_TRP'.format(LND_TYPE, ID))
+dta = pd.DataFrame(logbook)
+srv.exportLog(logbook, OUT_PTH, '{}_{}_LOG'.format(LND_TYPE, ID))
