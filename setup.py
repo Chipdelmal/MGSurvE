44c1b6bab0a59618c5f8ab71454b0d60e774b580
chipdelmal <chipdelmal@gmail.com>
2022-01-04 14:50:59 -0800

    Setup update for pytests

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         'numpy', 'matplotlib', 'ipython',
         'jupyter', 'pandas', 'compress-pickle',
         'deap', 'scikit-learn', 'scipy', 'vincenty',
-        'pytest', 'networkx'
+        'pytest', 'networkx', 'cartopy'
     ],
     license='MIT',
     classifiers=[
