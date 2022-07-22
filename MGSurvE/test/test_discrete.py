e367bea76a89083bde743b47c0e58fe80835aa2f
chipdelmal <chipdelmal@gmail.com>
2022-07-22 10:02:35 -0700

    Adding mutation unit tests for DO

--- MGSurvE/test/test_discrete.py
+++ MGSurvE/test/test_discrete.py
@@ -1,4 +1,7 @@
+
 import unittest
+import numpy as np
+from copy import deepcopy
 import MGSurvE as srv
 
 def test_InitDiscrete():
@@ -9,7 +12,6 @@ def test_InitDiscrete():
     lTest = (len(chrom) == len(vct))
     ixTest = ((vix-ban)==vix)
     assert (lTest and ixTest)
-
 
 def test_MutateDiscrete():
     (ub, chromSize) = (100, 100)
@@ -24,6 +26,40 @@ def test_MutateDiscrete():
     allZero = (len([i for i in chromB if i==0]) == len(chromB))
     assert (noZero and allZero)
 
+def test_DiscreteSelectiveMutation_OneShift():
+    trpsNum = 25
+    (trpsFxd, ptdsId) = ([0]*trpsNum, list(range(5000)))
+    initChrom = srv.initDiscreteChromosome(ptdsId, trpsFxd, banSites=None)
+    # Test one shift mask -----------------------------------------------------
+    results = []
+    for i in range(trpsNum):
+        trpsFxd = [0]*trpsNum
+        trpsFxd[i] = 1
+        mutChrom = deepcopy(initChrom)
+        srv.mutateDiscreteChromosome(
+            mutChrom, ptdsId, trpsFxd, 
+            indpb=1, banSites=None
+        )
+        pSum = np.sum([mutChrom[i]==initChrom[i] for i in range(trpsNum)])
+        results.append((1 <= pSum) and (pSum < 5))
+    assert all(results)
+
+def test_DiscreteSelectiveMutation_CumShift():
+    trpsNum = 25
+    (trpsFxd, ptdsId) = ([0]*trpsNum, list(range(5000)))
+    initChrom = srv.initDiscreteChromosome(ptdsId, trpsFxd, banSites=None)
+    # Test cumulative on mask -------------------------------------------------
+    results = []
+    for i in range(trpsNum):
+        trpsFxd[i] = 1
+        mutChrom = deepcopy(initChrom)
+        srv.mutateDiscreteChromosome(
+            mutChrom, ptdsId, trpsFxd, 
+            indpb=1, banSites=None
+        )
+        pSum = np.sum([mutChrom[i]==initChrom[i] for i in range(trpsNum)])
+        results.append((i <= pSum) and (pSum < i+5))
+    assert all(results)
 
 ###############################################################################
 # Main
