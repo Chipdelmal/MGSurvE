7ad83db9bdead1a45a7c950e5f15259bdbf2608b
chipdelmal <chipdelmal@gmail.com>
2022-08-05 14:33:03 -0700

    Updates on scripts for DO STP demo

--- MGSurvE/demos/Paper/STP.py
+++ MGSurvE/demos/Paper/STP.py
@@ -17,7 +17,7 @@ if srv.isNotebook():
     FXD_TRPS =  True
 else:
     FXD_TRPS =  (argv[1] == 'True')
-(TRPS_NUM, GENS) = (6, 1500)
+(TRPS_NUM, GENS) = (8, 1500)
 DIAG_VAL = 0.1
 ###############################################################################
 # Debugging fixed traps at land masses
@@ -177,7 +177,7 @@ pop = toolbox.populationCreator(n=POP_SIZE)
 hof = tools.HallOfFame(1)
 stats = tools.Statistics(lambda ind: ind.fitness.values)   
 stats.register("min", np.min)
-stats.register("avg", np.mean)
+stats.register("median", np.median)
 stats.register("max", np.max)
 stats.register("best", lambda fitnessValues: fitnessValues.index(min(fitnessValues)))
 stats.register("traps", lambda fitnessValues: pop[fitnessValues.index(min(fitnessValues))])
