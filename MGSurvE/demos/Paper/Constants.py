793383a52ed734722ea8c896079afc0a634c8a7e
chipdelmal <chipdelmal@gmail.com>
2023-03-06 09:51:11 -0800

    VAL Validation experiments updated

--- MGSurvE/demos/Paper/Constants.py
+++ MGSurvE/demos/Paper/Constants.py
@@ -5,29 +5,29 @@ import MGSurvE as srv
 # Path for outputs
 out_pth = './sims_out/'
 # Landscape's bounding box
-bbox = ((-300, 300), (-300, 300))
+bbox = ((-350, 350), (-350, 350))
 # Mosquito movement kernel
 mKerZ = {'params': [0.01848777, 1.0e-10, math.inf], 'zeroInflation': .75}
 mKerN = {'params': [0.01848777, 1.0e-10, math.inf], 'zeroInflation': 0}
 # Number of sites and clusters in the environment
-ptsNum = 200
+ptsNum = 300
 (clsNum, clsRad) = (5, 175)
 # Probability for each point-type
-pTypesProb =[0.025, 0.725, 0.25]
+pTypesProb =[0.1, 0.7, 0.2]
 # Number and type of traps
 nullTraps = [0, 0, 0, 0, 0]
-typeTraps = [0, 0, 0, 0, 0]
+typeTraps = [0, 0, 0, 1, 1]
 # Traps' kernels
 tKer = {
     0: {'kernel': srv.exponentialDecay, 'params': {'A': 0.80, 'b': .035}},
-    1: {'kernel': srv.exponentialDecay, 'params': {'A': 0.75, 'b': .025}},
-    2: {'kernel': srv.exponentialDecay, 'params': {'A': 1.00, 'b': .040}}
+    1: {'kernel': srv.exponentialDecay, 'params': {'A': 1.00, 'b': .040}},
+    2: {'kernel': srv.exponentialDecay, 'params': {'A': 0.75, 'b': .025}}
 }
 # Transition probabilities between point-types
 msk = [
-    [0.05, 0.70, 0.25],
-    [0.30, 0.10, 0.60],
-    [0.80, 0.10, 0.10],
+    [0.05, 0.90, 0.05],
+    [0.25, 0.05, 0.70],
+    [0.90, 0.05, 0.05],
 ]
 # GA Settings
 (gens, verbose) = (1500, True)
--- MGSurvE/demos/Paper/PaperExperiments.sh
+++ MGSurvE/demos/Paper/PaperExperiments.sh
@@ -1,70 +1,54 @@
 #!/bin/bash
 
 OPT=$1
-declare -a lnds=("Circle" "Grid" "Uniform" "Ring" "Poisson")
+declare -a lnds=("Grid" "Uniform" "Ring" "Circle" "Poisson")
 ###############################################################################
 # Setting landscapes up
 ###############################################################################
-echo "* [1/3] Generating landscapes"
-for lnd in ${lnds[@]}; do
-    printf "\r\tGenerating $lnd..."
-    python Landscape.py $lnd "ZI"
-    python Landscape.py $lnd "ZN"
-    printf "\r\033[K"
-done
+# echo "* [1/3] Generating landscapes"
+# for lnd in ${lnds[@]}; do
+#     printf "\r\tGenerating $lnd..."
+#     python Landscape.py $lnd "ZI"
+#     python Landscape.py $lnd "ZN"
+#     printf "\r\033[K"
+# done
 ###############################################################################
 # Optimizing Continuous Traps
 ###############################################################################
-# echo "* [2/3] Optimizing landscapes"
-# if [ $OPT == "Simple" ];
-# then
-#     for lnd in ${lnds[@]}; do
-#         printf "\r\tOptimizing (Simple DO) $lnd Zero-Inflated..."
-#         python Optimization-Simple.py "${lnd}_LND_HOM" "ZI"
-#         python Optimization-Simple.py "${lnd}_LND_HET" "ZI"
-#         printf "\r\tOptimizing (Simple DO) $lnd Non Zero-Inflated..."
-#         python Optimization-Simple.py "${lnd}_LND_HOM" "ZN"
-#         python Optimization-Simple.py "${lnd}_LND_HET" "ZN"
-#         printf "\r\033[K"
-#     done
-# else
-#     for lnd in ${lnds[@]}; do
-#         printf "\r\tOptimizing (Simple CO) $lnd Zero-Inflated..."
-#         python Optimization.py "${lnd}_LND_HOM" "ZI"
-#         python Optimization.py "${lnd}_LND_HET" "ZI"
-#         printf "\r\tOptimizing (Simple CO) $lnd Non Zero-Inflated..."
-#         python Optimization.py "${lnd}_LND_HOM" "ZN"
-#         python Optimization.py "${lnd}_LND_HET" "ZN"
-#         printf "\r\033[K"
-#     done
-# fi
-# ###############################################################################
-# # Optimizing Discrete Traps
-# ###############################################################################
+echo "* [2/3] Optimizing landscapes"
+if [ $OPT == "Simple" ];
+then
+    for lnd in ${lnds[@]}; do
+        python Optimization-Simple.py "${lnd}_LND_HOM" "ZI"
+        python Optimization-Simple.py "${lnd}_LND_HET" "ZI"
+        printf "\r\033[K"
+    done
+else
+    for lnd in ${lnds[@]}; do
+        python Optimization.py "${lnd}_LND_HOM" "ZI"
+        python Optimization.py "${lnd}_LND_HET" "ZI"
+        printf "\r\033[K"
+    done
+fi
+###############################################################################
+# Optimizing Discrete Traps
+###############################################################################
 # echo "* [3/3] Optimizing landscapes"
 # if [ $OPT == "Simple" ];
 # then
 #     for lnd in ${lnds[@]}; do
-#         printf "\r\tOptimizing (Complex DO) $lnd Zero-Inflated..."
-#         python OptimizationDO-Simple.py "${lnd}_LND_HOM" "ZI"
-#         python OptimizationDO-Simple.py "${lnd}_LND_HET" "ZI"
-#         printf "\r\tOptimizing (Complex DO) $lnd Non Zero-Inflated..."
 #         python OptimizationDO-Simple.py "${lnd}_LND_HOM" "ZI"
 #         python OptimizationDO-Simple.py "${lnd}_LND_HET" "ZI"
 #         printf "\r\033[K"
 #     done
 # else
 #     for lnd in ${lnds[@]}; do
-#         printf "\r\tOptimizing (Complex CO) $lnd Zero-Inflated..."
 #         python OptimizationDO.py "${lnd}_LND_HOM" "ZI"
 #         python OptimizationDO.py "${lnd}_LND_HET" "ZI"
-#         printf "\r\tOptimizing (Complex CO) $lnd Non Zero-Inflated..."
-#         python OptimizationDO.py "${lnd}_LND_HOM" "ZN"
-#         python OptimizationDO.py "${lnd}_LND_HET" "ZN"
 #         printf "\r\033[K"
 #     done
 # fi
-# ###############################################################################
-# # Goodbye
-# ###############################################################################
-# echo "* Done!"
+###############################################################################
+# Goodbye
+###############################################################################
+echo "* Done!"
