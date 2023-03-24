6994286a26eec8050540bdee0d315946c15abaf7
chipdelmal <chipdelmal@gmail.com>
2023-03-24 13:46:30 -0700

    YKN Setting up to run on dsk

--- MGSurvE/demos/Paper/YKN-Discrete.sh
+++ MGSurvE/demos/Paper/YKN-Discrete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for m in "sum" "man" "max"; do
-    echo "* Optimizing $m"
+    echo "* Optimizing $m..."
     for i in {1..30}; do
-        echo "* Running iteration $i"
+        echo "* Running iteration $i..."
         python YKN-Discrete.py "YKN" "$m" "$i"
     done
 done
