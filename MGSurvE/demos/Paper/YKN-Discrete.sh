cab73e3dbfa1aec2cd02be325cef5dbe3476ce46
chipdelmal <chipdelmal@gmail.com>
2023-03-24 14:06:28 -0700

    YKN Discrete bash script looping now

--- MGSurvE/demos/Paper/YKN-Discrete.sh
+++ MGSurvE/demos/Paper/YKN-Discrete.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+WHT='\033[0;37m'  
+RED='\033[0;31m'
+NCL='\033[0m'
+
 for m in "sum" "man" "max"; do
-    echo "* Optimizing $m..."
+    printf "${WHT}* Optimizing $m...${NCL}\n"
     for i in {1..30}; do
-        echo "* Running iteration $i..."
+        printf "${RED}\t* Running iteration $i... ${NCL}\n"
         python YKN-Discrete.py "YKN" "$m" "$i"
     done
 done
