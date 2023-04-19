42e7aed046eb0e217393ac24683dfc4ca02827bb
chipdelmal <chipdelmal@gmail.com>
2023-04-19 11:05:06 -0700

    YKN Video is now working

--- MGSurvE/demos/YKN_Validations/YKN-Video.py
+++ MGSurvE/demos/YKN_Validations/YKN-Video.py
@@ -14,13 +14,13 @@ import matplotlib.pyplot as plt
 import MGSurvE as srv
 import auxiliary as aux
 from PIL import Image
-# matplotlib.use('agg')
+matplotlib.use('agg')
 import warnings
 warnings.filterwarnings("ignore")
 
 
 if srv.isNotebook():
-    (ID, AP, RID) = ('YKND', 'man', '01')
+    (ID, AP, RID) = ('YKND', 'man', '02')
 else:
     (ID, AP, RID) = argv[1:]
 (GENS, RID, FPAT) = (5000, int(RID), ID+'-{}_16*')
@@ -29,7 +29,7 @@ else:
 ###############################################################################
 FNAME = FPAT.format(AP)[:-1]+'-{:02d}_'.format(RID)
 I_PTH = '/home/chipdelmal/Documents/WorkSims/MGSurvE_Validations/YKND_5000'
-O_PTH = path.join(I_PTH, 'video')
+O_PTH = path.join(I_PTH, 'VID_{}'.format(FNAME[:-1]))
 srv.makeFolder(O_PTH)
 ###############################################################################
 # Load Files
@@ -50,43 +50,60 @@ fig.savefig(
     transparent=False, facecolor='w',
     bbox_inches='tight', pad_inches=PAD, dpi=DPI
 )
+plt.close("all")
 ###############################################################################
 # Plot Optimization
 ###############################################################################
 fitFun = aux.switchFunction(AP)
-
 gen = 1000
-
-trpEntry = log.iloc[gen]['traps']
-if ID == 'YKND':
-    trpPos = aux.idStringToArray(trpEntry, discrete=True)
-    trpCds = srv.chromosomeIDtoXY(trpPos, lnd.pointID, lnd.pointCoords).T
-# Assemble and update traps ---------------------------------------------------
-trapsLocs = pd.DataFrame(
-    np.vstack([trpCds, lnd.trapsTypes, lnd.trapsFixed]).T, 
-    columns=['lon', 'lat', 't', 'f']
-)
-trapsLocs['t'] = trapsLocs['t'].astype('int64')
-trapsLocs['f'] = trapsLocs['f'].astype('int64')
-lnd.updateTraps(trapsLocs, lnd.trapsKernels)
-# Calculate new fitness -------------------------------------------------------
-if ID == 'YKND':
-    fitness = srv.calcDiscreteFitness(
-        trpPos, lnd, optimFunction=srv.getDaysTillTrapped,
-        optimFunctionArgs={'inner': np.sum, 'outer': fitFun}
-    )[0]
-    
-# Plot ------------------------------------------------------------------------  
-(fig, ax) = (plt.figure(figsize=FIGS), plt.axes(projection=PROJ))
-lnd.plotSites(fig, ax, size=50)
-lnd.updateTrapsRadii([0.250, 0.125, 0.100])
-lnd.plotTraps(
-    fig, ax, 
-    zorders=(30, 25), transparencyHex='55', 
-    latlon=True, proj=PROJ
-)
-srv.plotFitness(
-    fig, ax, fitness, 
-    fmt='{:.5f}', fontSize=20, color='#00000066', pos=(0.75, 0.10)
-)
-srv.plotClean(fig, ax, bbox=lnd.landLimits)
+for gen in range(GENS)[:]:
+    print("* {:04d}/{:04d}".format(gen, GENS), end='\n')
+    trpEntry = log.iloc[gen]['traps']
+    if ID == 'YKND':
+        trpPos = aux.idStringToArray(trpEntry, discrete=True)
+        trpCds = srv.chromosomeIDtoXY(trpPos, lnd.pointID, lnd.pointCoords).T
+    # Assemble and update traps -----------------------------------------------
+    trapsLocs = pd.DataFrame(
+        np.vstack([trpCds, lnd.trapsTypes, lnd.trapsFixed]).T, 
+        columns=['lon', 'lat', 't', 'f']
+    )
+    trapsLocs['t'] = trapsLocs['t'].astype('int64')
+    trapsLocs['f'] = trapsLocs['f'].astype('int64')
+    lnd.updateTraps(trapsLocs, lnd.trapsKernels)
+    lnd.updateTrapsRadii([0.250, 0.125, 0.100])
+    # Calculate new fitness ---------------------------------------------------
+    if ID == 'YKND':
+        fitness = srv.calcDiscreteFitness(
+            trpPos, lnd, optimFunction=srv.getDaysTillTrapped,
+            optimFunctionArgs={'inner': np.sum, 'outer': fitFun}
+        )[0]
+        
+    # Plot --------------------------------------------------------------------
+    (fig, ax) = (plt.figure(figsize=FIGS), plt.axes(projection=PROJ))
+    lnd.plotTraps(
+        fig, ax, 
+        zorders=(30, 25), transparencyHex='55', 
+        latlon=True, proj=PROJ
+    )
+    srv.plotFitness(
+        fig, ax, fitness, 
+        fmt='{:.5f}', fontSize=20, color='#00000066', pos=(0.75, 0.10)
+    )
+    srv.plotClean(fig, ax, bbox=lnd.landLimits)
+    fig.savefig(
+        path.join(O_PTH, '{:04d}.png'.format(gen)), 
+        transparent=True, facecolor=None,
+        bbox_inches='tight', pad_inches=PAD, dpi=DPI
+    )
+    plt.close("all")
+    # Overlay Brute-force -----------------------------------------------------
+    time.sleep(.25)
+    background = Image.open(path.join(O_PTH, FNAME+'CLN.png')).convert('RGBA')
+    foreground = Image.open(path.join(O_PTH, '{:04d}.png'.format(gen))).convert('RGBA')
+    (w, h) = background.size
+    background = background.crop((0, 0, w, h))
+    foreground = foreground.resize((int(w/1), int(h/1)), Image.ANTIALIAS)
+    background = Image.alpha_composite(background, foreground)
+    background.save(path.join(O_PTH, '{:04d}.png'.format(gen)), dpi=(DPI, DPI))
+    background.close()
+    foreground.close()
