b09ba04a6affd5c5525750cdfe7748b9bebd3210
chipdelmal <chipdelmal@gmail.com>
2023-04-24 14:06:51 -0700

    YKN Minor bash cycle update

--- MGSurvE/demos/YKN/YKN-Continuous.sh
+++ MGSurvE/demos/YKN/YKN-Continuous.sh
@@ -4,7 +4,7 @@ WHT='\033[0;37m'
 RED='\033[0;31m'
 NCL='\033[0m'
 
-for i in 1..10; do
+for i in {1..10}; do
     printf "${WHT}* Iteration $i...${NCL}\n" 
     for m in "max"; do # "max" "sum"; do 
         printf "${RED}\t* Optimizing $m... ${NCL}\n"
