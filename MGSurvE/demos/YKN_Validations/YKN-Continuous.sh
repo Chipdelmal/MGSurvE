79c8f9927334f2f0d8560db8784061ebd52c4a81
chipdelmal <chipdelmal@gmail.com>
2023-04-24 14:02:32 -0700

    Server merge

--- MGSurvE/demos/YKN_Validations/YKN-Continuous.sh
+++ MGSurvE/demos/YKN_Validations/YKN-Continuous.sh
@@ -4,7 +4,7 @@ WHT='\033[0;37m'
 RED='\033[0;31m'
 NCL='\033[0m'
 
-for i in 1 3 5; do
+for i in 5 6 7 8 9 10; do
     printf "${WHT}* Iteration $i...${NCL}\n" 
     for m in "man"; do # "max" "sum"; do 
         printf "${RED}\t* Optimizing $m... ${NCL}\n"
