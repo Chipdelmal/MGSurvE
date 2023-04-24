2d900a6dd8bbb7479208c9f95d1bc744eda7bb13
chipdelmal <chipdelmal@gmail.com>
2023-04-24 14:03:43 -0700

    Setting YKN continuous up

--- MGSurvE/demos/YKN/YKN-Continuous.sh
+++ MGSurvE/demos/YKN/YKN-Continuous.sh
@@ -4,9 +4,9 @@ WHT='\033[0;37m'
 RED='\033[0;31m'
 NCL='\033[0m'
 
-for i in 5 6 7 8 9 10; do
+for i in 1..10; do
     printf "${WHT}* Iteration $i...${NCL}\n" 
-    for m in "man"; do # "max" "sum"; do 
+    for m in "max"; do # "max" "sum"; do 
         printf "${RED}\t* Optimizing $m... ${NCL}\n"
         python YKN-Continuous.py "YKN" "$m" "$i"
     done
