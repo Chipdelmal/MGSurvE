41b04c681c30b9d2418336a3c8ccfbb7af77938c
chipdelmal <chipdelmal@gmail.com>
2023-09-06 14:08:46 -0700

    Timing benchmark is almost done

---
+++ MGSurvE/benchmarks/auxiliary.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+from scipy.interpolate import griddata
+
+
+def axisRange(x):
+    return [min(x), max(x)]
+
+
+def calcResponseSurface(
+    iX, iY, dZ,
+    scalers=(1, 1, 1), mthd='linear',
+    xAxis='linear', yAxis='linear',
+    xLogMin=1e-10, yLogMin=1e-10,
+    DXY=(5000, 5000)
+):
+    (xN, yN, zN) = (
+            np.array([float(i/scalers[0]) for i in iX]),
+            np.array([float(i/scalers[1]) for i in iY]),
+            np.array([float(i/scalers[2]) for i in dZ])
+        )
+    (xRan, yRan, zRan) = (axisRange(i) for i in (xN, yN, zN))
+    # X-Axis scale ------------------------------------------------------------
+    if xAxis=='linear':
+        xi = np.linspace(xRan[0], xRan[1], DXY[0])
+    elif xAxis=='log':
+        if xRan[0] == 0:
+            xRan[0] = xLogMin
+        xi = np.geomspace(xRan[0], xRan[1], DXY[0])
+    # Y-Axis scale ------------------------------------------------------------
+    if yAxis=='linear':
+        yi = np.linspace(yRan[0], yRan[1], DXY[1])
+    elif yAxis=='log':
+        if yRan[0] == 0:
+            yRan[0] =  yLogMin
+        yi = np.geomspace(yRan[0], yRan[1], DXY[1])
+    # Grid --------------------------------------------------------------------
+    zi = griddata((xN, yN), zN, (xi[None, :], yi[:, None]), method=mthd)
+    # Return variables
+    ranges = (xRan, yRan, zRan)
+    grid = (xN, yN, zN)
+    surf = (xi, yi, zi)
+    return {'ranges': ranges, 'grid': grid, 'surface': surf}
--- MGSurvE/benchmarks/bch_Regular.py
+++ MGSurvE/benchmarks/bch_Regular.py
@@ -14,17 +14,15 @@ os.environ["NUMEXPR_NUM_THREADS"] = str(CORES)
 # Load libraries --------------------------------------------------------------
 import warnings
 warnings.filterwarnings("ignore")
-import math
 import warnings
 import numpy as np
 import pandas as pd
 from os import path
-from sys import argv
-from copy import deepcopy
 from time import perf_counter
 from itertools import product
 import matplotlib.pyplot as plt
 from compress_pickle import dump, load
+import auxiliary as aux
 import MGSurvE as srv
 
 # Constants for output --------------------------------------------------------
@@ -32,8 +30,9 @@ PTH_O = './sims_out/'
 BBOX = ((-100, 100), (-100, 100))
 srv.makeFolder(PTH_O)
 # Experiment constants --------------------------------------------------------
-(GENS, REPS) = (10, 5)
-(PTS_RAN, TRP_RAN) = ((3, 10, 2), (1, 5, 1))
+(GENS, REPS, DISCRETE) = (50, 10, True)
+(PTS_RAN, TRP_RAN) = ((10, 100, 10), (1, 10, 1))
+SUM_STAT = np.median
 ###############################################################################
 # Generate factorial tuples
 ###############################################################################
@@ -49,7 +48,7 @@ ix = 0
 (ptsNum, trpNum) = FACTORIAL[ix]
 for (ptsNum, trpNum) in FACTORIAL:
     # Setup sites -------------------------------------------------------------
-    xy = srv.ptsRegularGrid(ptsNum, (BBOX[0], BBOX[0])).T
+    xy = srv.ptsRandUniform(ptsNum, BBOX).T
     pts = pd.DataFrame({'x': xy[0], 'y': xy[1], 't': [0]*xy.shape[1]})
     # Setup traps -------------------------------------------------------------
     nt= [0]*trpNum
@@ -61,16 +60,40 @@ for (ptsNum, trpNum) in FACTORIAL:
     timings = []
     for rep in range(REPS):
         t0_rep = perf_counter()
-        (lnd, logbook) = srv.optimizeTrapsGA(
-            lnd, generations=GENS, pop_size='auto', verbose=False,
-            mating_params='auto', mutation_params='auto', 
-            selection_params='auto', fitFuns={'inner': np.sum, 'outer': np.max}
-        )
+        if not DISCRETE:
+            (lnd, logbook) = srv.optimizeTrapsGA(
+                lnd, generations=GENS, pop_size='auto', verbose=False,
+                mating_params='auto', mutation_params='auto', 
+                selection_params='auto', fitFuns={'inner': np.sum, 'outer': np.max}
+            )
+        else:
+            (lnd, logbook) = srv.optimizeDiscreteTrapsGA(
+                lnd, generations=GENS, pop_size='auto', verbose=False,
+                mating_params='auto', mutation_params='auto', 
+                selection_params='auto', fitFuns={'inner': np.sum, 'outer': np.max}
+            )
         tf_rep = perf_counter()
         timings.append(tf_rep-t0_rep)
-    TIME[(ptsNum, trpNum)] = timings
-    dump(TIME, path.join(PTH_O, 'timings.bz2'))
+    TIME[(lnd.pointNumber, lnd.trapsNumber)] = timings
+    # Export results ----------------------------------------------------------
+    app = ('DSC' if DISCRETE else 'CNT')
+    dump(TIME, path.join(PTH_O, f'timings_{app}.bz2'))
 ###############################################################################
 # Analyze resulting dictionary
 ###############################################################################
-load(path.join(PTH_O, 'timings.bz2'))
+app = ('DSC' if DISCRETE else 'CNT')
+TIME = load(path.join(PTH_O, f'timings_{app}.bz2'))
+cmap = srv.colorPaletteFromHexList(['#ffffff', '#8093f1'])
+# Plot ------------------------------------------------------------------------
+(x, y) = np.array(list(TIME.keys())).T
+z = np.array([SUM_STAT(i) for i in TIME.values()])
+rs = aux.calcResponseSurface(x, y, z)
+(a, b) = ((min(x), max(x)), (min(y), max(y)))
+(ran, rsG, rsS) = (rs['ranges'], rs['grid'], rs['surface'])
+(fig, ax) = plt.subplots(figsize=(10, 10))
+xy = ax.plot(rsG[0], rsG[1], 'k.', ms=5, alpha=.75, marker='x')
+cc = ax.contour(rsS[0], rsS[1], rsS[2], colors='#000000', linewidths=.5, alpha=1)
+cs = ax.contourf(rsS[0], rsS[1], rsS[2], cmap=cmap, extend='max')
+ax.set_xlabel("Number of Sites")
+ax.set_xlabel("Number of Traps")
+ax.colorbar()
